df2ad60e1ec4d408f976f8eaa381fc492be6f2c2
Fabio Beti <fabio.beti@edu.tbz.ch>
2022-12-14 13:47:02 +0100

    added changes

--- bin/myscript.sh
+++ bin/myscript.sh
@@ -9,7 +9,7 @@ date +%H:%M:%S >> $HTMLOUTPUTFILE
 ls /tmp >> $HTMLOUTPUTFILE
 echo "</body></html>" >> $HTMLOUTPUTFILE
 
- if [ $# -eq 1 ];
+ if [ "$#" -eq 1 ];
  then
     if [ -d "$1" ];
     then
