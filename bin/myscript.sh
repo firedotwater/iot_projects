e8944dd7db938245ccc4a37969cd7a50bcbe96f3
Fabio Beti <fabio.beti@edu.tbz.ch>
2022-12-14 10:40:36 +0100

    added changes

--- bin/myscript.sh
+++ bin/myscript.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-touch /opt/iotproject/index.html
-echo "<html><body>" >> /opt/iotproject/index.html
-echo "<h1>Mein Webserver</h1>" >> /opt/iotproject/index.html
-date +%H:%M:%S >> /opt/iotproject/index.html
-ls /tmp >> /opt/iotproject/index.html
-echo "</body></html>" >> /opt/iotproject/index.html
+PATH="/opt/iotproject/index.html"
+
+touch $PATH
+echo "<html><body>" >> $PATH
+echo "<h1>Mein Webserver</h1>" >> $PATH
+date +%H:%M:%S >> $PATH
+ls /tmp >> /$PATH
+echo "</body></html>" >> $PATH
 echo '$1='$1
 ls -ls $1
