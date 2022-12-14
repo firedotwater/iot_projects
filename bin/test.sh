d7107e0554877fd01f97063ece831b6d6336210a
Fabio Beti <fabio.beti@edu.tbz.ch>
2022-12-14 13:51:40 +0100

    added changes

--- bin/test.sh
+++ bin/test.sh
@@ -5,3 +5,18 @@ echo 2: $2
 echo 3: $3
 echo 4: $4
 cat $*
+
+
+if [ -z "$#" ]; then
+  echo "Variable is empty and will give the output of /tmp"
+  ls -l $DIRECTORYTOLIST
+  if [ -d "$1" ]; then
+    echo "Variable is a directory"
+  else
+    echo "Variable is not a directory ERROR"
+    exit 1
+  fi
+  else
+   echo "Variable is not empty" 
+   ls -ls $1
+ fi
