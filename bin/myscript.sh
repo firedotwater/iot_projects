0b4ae14dea9bf3ad134772aaf06bbd19aae27ffc
Fabio Beti <fabio.beti@edu.tbz.ch>
2022-12-14 11:01:05 +0100

    added changes

--- bin/myscript.sh
+++ bin/myscript.sh
@@ -7,11 +7,14 @@ echo "<h1>Mein Webserver</h1>" >> $path
 date +%H:%M:%S >> $path
 ls /tmp >> $path
 echo "</body></html>" >> $path
-echo '$1='$1
 
 if [ -z "$1" ]; then
   echo "Variable is empty and will give the output of /tmp"
   ls -ls /tmp/
+  if [ -d $1 ]; then
+  echo "Variable is a directory"
+  else
+  echo "Variable is not a directory"
 else
   echo "Variable is not empty"
   ls -ls $1
